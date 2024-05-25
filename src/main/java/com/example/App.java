8ca373f10e9ed4e6aea851ede49e6030ea875c2b
Vaibhav2031 <shetkarvaibhav78@gmail.com>
2024-05-25 20:38:43 +0530

    file split logic using executor

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.example.file_spliter.LogFileSplit;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src/logs/parent.log");
+
+        LogFileSplit logFileSplit = new LogFileSplit();
+        try {
+            logFileSplit.splitFile(filePath, 100);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
